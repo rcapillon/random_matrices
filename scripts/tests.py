d96707bf5ab7840c7f9da699f4bfbc4a2ae5e746
Rémi Capillon <rcapillon@yahoo.fr>
2025-08-14 14:19:51 +0200

    initial commit

---
+++ scripts/tests.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
+
+import src.random_matrices.generators
+
+
+if __name__ == '__main__':
+    a = 0.
---
+++ src/random_matrices/generators.py
@@ -0,0 +1,6 @@
+import numpy as np
+import scipy
+
+
+def G_0(dispersion_coeff, n_samples):
+    """"""
