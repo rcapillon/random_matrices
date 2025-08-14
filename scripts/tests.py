d9d548b8071ae8347764317ffe7b7466d92c69c3
Rémi Capillon <rcapillon@yahoo.fr>
2025-08-14 16:23:01 +0200

    basic sample generation test

--- scripts/tests.py
+++ scripts/tests.py
@@ -3,8 +3,13 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 
-import src.random_matrices.generators
+import src.random_matrices.generators as gen
 
 
 if __name__ == '__main__':
-    a = 0.
+    N = 10
+    dispersion_coeff = 0.2
+    n_samples = 100
+
+    mats_GOE = gen.GOE(N, dispersion_coeff, n_samples)
+    mats_G_0 = gen.SG_0_plus(N, dispersion_coeff, n_samples)
